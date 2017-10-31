ea2a20ad5df531b0e65f5e99826e1d52b9d50a2b
Bo <gebo@outlook.com>
2017-10-31 19:02:01 +0800

    Base singlaton

---
+++ sell/src/components/header/header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="header">
+    hello world, header!!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header'
+};
+</script>
+
+<style lang="stylus" rel='stylesheet/stylus'></style>
--- sell/src/main.js
+++ sell/src/main.js
@@ -1,15 +1,42 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 import App from './App';
-import router from './router';
+import goods from '@/components/goods/goods.vue'; // webpack.base.conf的配置 resolve里面alias
+import ratings from '@/components/ratings/ratings.vue';
+import seller from '@/components/seller/seller.vue';
 
-Vue.config.productionTip = false;
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+const routes = [{
+  path: '/',
+  redirect: '/goods'
+},
+{
+  path: '/goods',
+  component: goods
+},
+{
+  path: '/ratings',
+  component: ratings
+},
+{
+  path: '/seller',
+  component: seller
+}];
+
+// linkActiveClass: 选中的tab被激活应用的样式
+const router = new VueRouter({
+  linkActiveClass: 'active',
+  routes
+});
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
+  render: h => h(App)
 });
